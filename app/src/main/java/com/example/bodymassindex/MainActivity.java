18fcecd28253847cd22530fc38c06ec1055693e2
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:46:14 +0545

    validation completed

--- app/src/main/java/com/example/bodymassindex/MainActivity.java
+++ app/src/main/java/com/example/bodymassindex/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bodymassindex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,17 +30,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnCalculate){
-            BMI_Suman bmi = new BMI_Suman();
-            float heightInMeter = Float.parseFloat(etHeight.getText().toString())/100;
-            bmi.setHeight(heightInMeter);
-            bmi.setWeight(Float.parseFloat(etWeight.getText().toString()));
-            float result = bmi.calculateBMI();
+            if (validate()){
+                BMI_Suman bmi = new BMI_Suman();
+                float heightInMeter = Float.parseFloat(etHeight.getText().toString())/100;
+                bmi.setHeight(heightInMeter);
+                bmi.setWeight(Float.parseFloat(etWeight.getText().toString()));
+                float result = bmi.calculateBMI();
 
-            tvResult.setText(Float.toString(result));
-            if (result < 18.5) Toast.makeText(this,"Underweight", Toast.LENGTH_LONG).show();
-            if (result> 18.5 && result < 24.9) Toast.makeText(this,"Normal Weight", Toast.LENGTH_LONG).show();
-            if (result> 25 && result < 29.9) Toast.makeText(this,"Over Weight", Toast.LENGTH_LONG).show();
-            if (result >= 30) Toast.makeText(this,"Obesity", Toast.LENGTH_LONG).show();
+                tvResult.setText(Float.toString(result));
+                if (result < 18.5) Toast.makeText(this,"Underweight", Toast.LENGTH_LONG).show();
+                if (result> 18.5 && result < 24.9) Toast.makeText(this,"Normal Weight", Toast.LENGTH_LONG).show();
+                if (result> 25 && result < 29.9) Toast.makeText(this,"Over Weight", Toast.LENGTH_LONG).show();
+                if (result >= 30) Toast.makeText(this,"Obesity", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+    public Boolean validate(){
+        boolean validation = true;
+        if (TextUtils.isEmpty(etWeight.getText().toString())){
+            etWeight.setError("Please enter the weight");
+            etWeight.requestFocus();
+            validation = false;
+        }
+        if (TextUtils.isEmpty(etHeight.getText().toString())){
+            etHeight.setError("Please enter the height");
+            etHeight.requestFocus();
+            validation = false;
+        }
+        return validation;
     }
 }
